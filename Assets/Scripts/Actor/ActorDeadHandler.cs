3d05fc1faa57ee6f347a44e3980409c329e3b382
PhucDev02 <76049784+PhucDev02@users.noreply.github.com>
2024-12-03 23:45:41 +0700

    update

--- Assets/Scripts/Actor/ActorDeadHandler.cs
+++ Assets/Scripts/Actor/ActorDeadHandler.cs
@@ -3,15 +3,16 @@ using UnityEngine;
 public class ActorDeadHandler : ActorComponent
 {
     [SerializeField] GameObject deadIcon;
-
+    float time = 0;
     private void LateUpdate()
     {
         Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, Vector2.one, 0, -1);
         if (hits.Length >= 3)
         {
+            time += Time.deltaTime;
             foreach(var hit in hits)
             {
-                if(hit.CompareTag("MoveObstacle"))
+                if(hit.CompareTag("MoveObstacle")&&time>GameConfig.data.maxStayTime)
                 {
                     deadIcon.SetActive(true);
                     actor.Die();
@@ -20,5 +21,8 @@ public class ActorDeadHandler : ActorComponent
             }
         }
     }
-
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        time = 0;
+    }
 }
